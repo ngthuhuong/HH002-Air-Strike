488739e959f19417ba9a4cae1a305f28226f66e3
TrinhThanhNam <ttnam957@gmail.com>
2025-05-24 09:54:58 +0700

    add: new font, gui_pack

---
+++ Assets/_Project/Scripts/GUI/GUIProfile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIProfile : MonoBehaviour
+{
+    #region MonoBehaviour
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
